4b93a594c4345700e18674ae92a825f4c6159a6c
retr0-r00t <enzoniches@gmail.com>
2026-08-09 00:08:33 -0300

    Query Methods (jpql and native query)

--- src/main/java/com/nixs/spring_data/database/repository/IExerciciosRepository.java
+++ src/main/java/com/nixs/spring_data/database/repository/IExerciciosRepository.java
@@ -2,11 +2,30 @@ package com.nixs.spring_data.database.repository;
 
 import com.nixs.spring_data.database.model.ExcerciciosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IExerciciosRepository extends JpaRepository<ExcerciciosEntity, Integer> {
 
     List<ExcerciciosEntity> findAllByGrupoMuscular(String grupoMuscular);
+
+    // Em JPQL
+    @Query(value = """
+        SELECT e
+        FROM ExerciciosEntity e
+        WHERE UPPER(e.grupoMuscular) = UPPER(:grupoMuscular)
+""")
+    List<ExcerciciosEntity> findAllByGrupoMusucularJpql(@Param("grupoMuscular") String grupoMuscular);
+
+    // Em Native Query
+    @NativeQuery(value = """
+        SELECT e
+        FROM exercicios e
+        WHERE UPPER(e.grupo_muscular) = UPPER(:grupoMuscular)
+""")
+    List<ExcerciciosEntity> findAllByGrupoMusucularNative(@Param("grupoMuscular") String grupoMuscular);
 
 }
